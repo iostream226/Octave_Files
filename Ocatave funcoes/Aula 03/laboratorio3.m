f01e12d8d1e79cf190afe1d87f7fa769bac9218e
iostream226 <94626024+iostream226@users.noreply.github.com>
2023-05-18 10:53:28 -0300

    Add files via upload

---
+++ Ocatave funcoes/Aula 03/laboratorio3.m
@@ -0,0 +1,29 @@
+# Created by Octave 5.1.0, Thu May 18 10:52:52 2023 GMT <unknown@lmsii-18>
+# name: A
+# type: matrix
+# rows: 1
+# columns: 5
+ -1 0 1 2 3
+
+
+# name: k
+# type: matrix
+# rows: 1
+# columns: 7
+ -3 -2 -1 0 1 2 3
+
+
+# name: t
+# type: matrix
+# rows: 1
+# columns: 7
+ -3 -2 -1 0 1 2 3
+
+
+# name: x
+# type: matrix
+# rows: 1
+# columns: 7
+ 1 3 2 1 -1 2 1
+
+
